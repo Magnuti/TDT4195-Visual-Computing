94b4752f48935e01214a08244304b08ea045dbc4
Magnus <magnustidemann@gmail.com>
2020-10-12 20:17:31 +0200

    Task 2a and 2b completed

---
+++ image_processing/assignment_1/README.md
@@ -0,0 +1,10 @@
+```
+conda install pytorch torchvision cpuonly -c pytorch
+```
+
+For CUDA and CUDNN we also need these:
+
+```
+conda install tqdm
+conda install -c conda-forge scikit-image
+```
--- image_processing/assignment_1/task2ab.py
+++ image_processing/assignment_1/task2ab.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 import pathlib
 from utils import read_im, save_im
+import numpy as np
+
 output_dir = pathlib.Path("image_solutions")
 output_dir.mkdir(exist_ok=True)
 
 
 im = read_im(pathlib.Path("images", "lake.jpg"))
 plt.imshow(im)
+# plt.show()
 
 
 def greyscale(im):
@@ -19,12 +22,19 @@ def greyscale(im):
         im ([type]): [np.array of shape [H, W]]
     """
 
-    return im
+    # grey = 0.212R + 0.7152G + 0.0722B
+    return im.dot([0.212, 0.7152, 0.0722])
+
+    # Without weights:
+    # return np.sum(a=im, axis=2)
 
 
 im_greyscale = greyscale(im)
 save_im(output_dir.joinpath("lake_greyscale.jpg"), im_greyscale, cmap="gray")
 plt.imshow(im_greyscale, cmap="gray")
+# plt.show()
+
+# print("Image range: {}-{} ".format(im.min(), im.max()))
 
 
 def inverse(im):
@@ -36,5 +46,12 @@ def inverse(im):
     Returns:
         im ([type]): [np.array of shape [H, W]]
     """
-    # YOUR CODE HERE
-    return im
+
+    return 1.0 - im
+
+
+im_inversed = inverse(im_greyscale)
+save_im(output_dir.joinpath("lake_greyscale_inversed.jpg"),
+        im_inversed, cmap="gray")
+plt.imshow(im_inversed, cmap="gray")
+# plt.show()
