88c0c1bd88d79845046c5e4a8e74b9a2469c4fea
Magnus <magnustidemann@gmail.com>
2020-10-13 10:27:13 +0200

    Task 2c completed

--- image_processing/assignment_1/task2c.py
+++ image_processing/assignment_1/task2c.py
@@ -22,11 +22,33 @@ def convolve_im(im, kernel,
         [type]: [np.array of shape [H, W, 3]. should be same as im]
     """
     assert len(im.shape) == 3
+    image_height = im.shape[0]
+    image_width = im.shape[1]
+    kernel_dim_divided = kernel.shape[0] // 2
 
-    return im
+    computed = np.zeros(shape=(image_height, image_width, 3))
 
+    for h_index in range(image_height):
+        for w_index in range(image_width):
+            for channel_index in range(im.shape[2]):
+                pixel_value = 0.0
+                for ki in range(-kernel_dim_divided, kernel_dim_divided + 1):
+                    for kj in range(-kernel_dim_divided, kernel_dim_divided + 1):
+                        h_coor = h_index + ki
+                        if h_coor == -1 or h_coor >= image_height:
+                            continue
+                        w_coor = w_index + kj
+                        if w_coor == -1 or w_coor >= image_width:
+                            continue
+                        if kj == 0 and ki == 0:
+                            continue
+                        pixel_value += kernel[ki, kj] * \
+                            im[h_coor, w_coor, channel_index]
+                computed[h_index, w_index, channel_index] = pixel_value
 
-# Define the convolutional kernels
+    return computed
+
+    # Define the convolutional kernels
 h_a = np.ones((3, 3)) / 9
 sobel_x = np.array([
     [-1, 0, 1],
@@ -51,7 +73,8 @@ assert im_sobel.shape == im.shape,     f"Expected smoothed im ({im_sobel.shape}"
 
 plt.subplot(1, 2, 1)
 plt.imshow(normalize(im_smoothed))
+# plt.show()
 
 plt.subplot(1, 2, 2)
 plt.imshow(normalize(im_sobel))
-plt.show()
+# plt.show()
