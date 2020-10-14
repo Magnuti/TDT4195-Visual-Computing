093f3d98244e03ab6458cfcbaddb6157f1b20f42
Magnus <magnustidemann@gmail.com>
2020-10-14 20:53:25 +0200

    Task 4 a and b completed

--- image_processing/assignment_1/README.md
+++ image_processing/assignment_1/README.md
@@ -1,10 +1,9 @@
 ```
-conda install pytorch torchvision cpuonly -c pytorch
-```
-
-For CUDA and CUDNN we also need these:
-
-```
 conda install tqdm
 conda install -c conda-forge scikit-image
 ```
+PyTorch with CUDA:
+```
+conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
+```
+If we want to use the CPU only (not CUDA) we can run this command instead of the above: `$ conda install pytorch torchvision cpuonly -c pytorch`
--- image_processing/assignment_1/task4a.py
+++ image_processing/assignment_1/task4a.py
@@ -1,3 +1,4 @@
+import pathlib
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
@@ -14,8 +15,10 @@ np.random.seed(0)
 # Load the dataset and print some stats
 batch_size = 64
 
+# TODO Make sure all future uses are normalized before delivery, expect first one on task a
 image_transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
+    # torchvision.transforms.Normalize(mean=0.5, std=0.5)
 ])
 
 dataloader_train, dataloader_test = dataloaders.load_dataset(
@@ -31,7 +34,7 @@ def create_model():
     """
     model = nn.Sequential(
         nn.Flatten(),  # Flattens the image from shape (batch_size, C, Height, width) to (batch_size, C*height*width)
-        nn.Linear(28*28*1, 10)
+        nn.Linear(28*28*1, 10)  # 28*28 input features, 10 outputs
         # No need to include softmax, as this is already combined in the loss function
     )
     # Transfer model to GPU memory if a GPU is available
@@ -41,13 +44,15 @@ def create_model():
 
 model = create_model()
 
-
 # Test if the model is able to do a single forward pass
 example_images = utils.to_cuda(example_images)
 output = model(example_images)
 print("Output shape:", output.shape)
 expected_shape = (batch_size, 10)  # 10 since mnist has 10 different classes
 assert output.shape == expected_shape,    f"Expected shape: {expected_shape}, but got: {output.shape}"
+
+print("Using CUDA:", torch.cuda.is_available())
+print("Current device:", torch.cuda.current_device())
 
 
 # Hyperparameters
@@ -61,7 +66,6 @@ loss_function = torch.nn.CrossEntropyLoss()
 # Define optimizer (Stochastic Gradient Descent)
 optimizer = torch.optim.SGD(model.parameters(),
                             lr=learning_rate)
-
 
 trainer = Trainer(
     model=model,
@@ -86,7 +90,8 @@ plt.xlabel("Global Training Step")
 plt.ylabel("Cross Entropy Loss")
 plt.savefig("image_solutions/task_4a.png")
 
-plt.show()
+# plt.show()
+plt.clf()
 
 torch.save(model.state_dict(), "saved_model.torch")
 final_loss, final_acc = utils.compute_loss_and_accuracy(
@@ -94,23 +99,21 @@ final_loss, final_acc = utils.compute_loss_and_accuracy(
 print(f"Final Test loss: {final_loss}. Final Test accuracy: {final_acc}")
 
 
-# You can delete the remaining code of this notebook, as this is just to illustrate one method to solve the assignment tasks.
+# * Training normalized starts here
 
-
-# This example code is here to illustrate how you can plot two different models to compare them.
-# Lets change a small part of our model: the number of epochs trained (NOTE, use 5 epochs for your experiments in the assignment.)
+image_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=0.5, std=0.5)
+])
 
 # We reset the manual seed to 0, such that the model parameters are initialized with the same random number generator.
 torch.random.manual_seed(0)
 np.random.seed(0)
 
-
 dataloader_train, dataloader_test = dataloaders.load_dataset(
     batch_size, image_transform)
-model = create_model()
 
-learning_rate = .0192
-num_epochs = 6
+model = create_model()
 
 # Redefine optimizer, as we have a new model.
 optimizer = torch.optim.SGD(model.parameters(),
@@ -123,17 +126,17 @@ trainer = Trainer(
     loss_function=loss_function,
     optimizer=optimizer
 )
-train_loss_dict_6epochs, test_loss_dict_6epochs = trainer.train(num_epochs)
-num_epochs = 5
+train_loss_dict_normalized, test_loss_dict_normalized = trainer.train(
+    num_epochs)
 
 
 # We can now plot the two models against eachother
 
 # Plot loss
-utils.plot_loss(train_loss_dict_6epochs,
-                label="Train Loss - Model trained with 6 epochs")
-utils.plot_loss(test_loss_dict_6epochs,
-                label="Test Loss - Model trained with 6 epochs")
+utils.plot_loss(train_loss_dict_normalized,
+                label="Train Loss - Model trained - normalized")
+utils.plot_loss(test_loss_dict_normalized,
+                label="Test Loss - Model trained - normalized")
 utils.plot_loss(train_loss_dict, label="Train Loss - Original model")
 utils.plot_loss(test_loss_dict, label="Test Loss - Original model")
 # Limit the y-axis of the plot (The range should not be increased!)
@@ -141,7 +144,7 @@ plt.ylim([0, 1])
 plt.legend()
 plt.xlabel("Global Training Step")
 plt.ylabel("Cross Entropy Loss")
-plt.savefig("image_solutions/task_4a.png")
+plt.savefig("image_solutions/task_4a_compared_to_normalized.png")
 
 plt.show()
 
@@ -149,3 +152,9 @@ torch.save(model.state_dict(), "saved_model.torch")
 final_loss, final_acc = utils.compute_loss_and_accuracy(
     dataloader_test, model, loss_function)
 print(f"Final Test loss: {final_loss}. Final Test accuracy: {final_acc}")
+
+
+# Save the model so we can use it in Task B without re-training it every time
+output_dir = pathlib.Path("models")
+output_dir.mkdir(exist_ok=True)
+torch.save(model, output_dir.joinpath("model.pt"))
