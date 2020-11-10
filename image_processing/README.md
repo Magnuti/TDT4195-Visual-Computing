cddf41bdd8ad9bf52ddab7f85a50ba12ff958f5f
Magnus <magnustidemann@gmail.com>
2020-11-10 14:07:41 +0100

    Task 2a completed

--- image_processing/README.md
+++ image_processing/README.md
@@ -3,3 +3,5 @@
 Assignment 1 is about image processing with filters/kernels and basic neural network image classification over the MNIST dataset.
 
 Assignment 2 is about classifying images with CNNs and image filtering in the frequency domain.
+
+Assignment 3 explores how we can segment an image into foreground and background by using basic segmentation algorithms, such as thresholding and region growing. It also expolores how we can use binary morphological operations to manipulate the contents of a binary image.
--- image_processing/assignment_3/task2a.py
+++ image_processing/assignment_3/task2a.py
@@ -2,6 +2,7 @@ import numpy as np
 import skimage
 import utils
 import pathlib
+import matplotlib.pyplot as plt
 
 
 def otsu_thresholding(im: np.ndarray) -> int:
@@ -18,7 +19,65 @@ def otsu_thresholding(im: np.ndarray) -> int:
     # START YOUR CODE HERE ### (You can change anything inside this block)
     # You can also define other helper functions
     # Compute normalized histogram
+
+    INTENSITY_RANGE = 255  # Number of grayscale values [0-255]
+
     threshold = 128
+    hist, bins = np.histogram(im, bins=INTENSITY_RANGE)
+
+    # Normalize hist
+    # New array so we don't mix up float and ints
+    hist_normalized = np.empty(INTENSITY_RANGE, dtype=float)
+    number_of_pixels = im.shape[0] * im.shape[1]
+    for i in range(len(hist)):
+        hist_normalized[i] = float(hist[i]) / float(number_of_pixels)
+
+    # Cumulative sums
+    cumulative_sums = np.cumsum(hist_normalized)
+
+    cumulative_means = np.empty(INTENSITY_RANGE, dtype=float)
+    cumulative_means[0] = 0
+    # Cumulative means
+    for i in range(1, len(hist_normalized)):
+        cumulative_means[i] = cumulative_means[i - 1] + i * hist_normalized[i]
+
+    # Global mean
+    global_mean = 0
+    for i in range(len(hist_normalized)):
+        global_mean += i * hist_normalized[i]
+
+    # Between-class variances
+    between_class_variances = np.empty(INTENSITY_RANGE, dtype=float)
+    between_class_variances = (global_mean * cumulative_sums -
+                               cumulative_means)**2 / (cumulative_sums * (1 - cumulative_sums))
+
+    # Find Otsu threshold from maximum values in the between-class variances
+    max_indexes = []
+    max_value = max(between_class_variances)
+    for i in range(len(between_class_variances)):
+        if(between_class_variances[i] == max_value):
+            max_indexes.append(i)
+
+    threshold = sum(max_indexes) / len(max_indexes)
+
+    # plt.figure(figsize=(16, 5))
+    # columns = 1
+    # rows = 4
+    # plt.subplot(columns, rows, 1)
+    # plt.plot(hist_normalized)
+    # plt.title("Hist normalized")
+    # plt.subplot(columns, rows, 2)
+    # plt.plot(cumulative_sums)
+    # plt.title("Cumulative sums")
+    # plt.subplot(columns, rows, 3)
+    # plt.plot(cumulative_means)
+    # plt.title("Cumulative means")
+    # plt.subplot(columns, rows, 4)
+    # plt.plot(between_class_variances)
+    # plt.title("Between-class variance")
+    # plt.savefig("image_processed/task2a-full{}.png".format(round(threshold, 0)))
+    # plt.show()
+
     return threshold
     ### END YOUR CODE HERE ###
 
