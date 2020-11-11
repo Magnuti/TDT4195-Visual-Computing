7412c63a599d601fcf7d74418f8b73d3c6921a30
Magnus <magnustidemann@gmail.com>
2020-11-11 15:46:32 +0100

    Task 3a complete

--- image_processing/assignment_3/task3a.py
+++ image_processing/assignment_3/task3a.py
@@ -1,7 +1,8 @@
 import utils
 import skimage
-import skimage.morphology
+import skimage.morphology as morph
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def remove_noise(im: np.ndarray) -> np.ndarray:
@@ -14,6 +15,53 @@ def remove_noise(im: np.ndarray) -> np.ndarray:
     """
     # START YOUR CODE HERE ### (You can change anything inside this block)
     # You can also define other helper functions
+
+    # An erosion radius of 7 is the lowest radius which remove all outliers
+    erosion_radius = 7
+    # A dilation radius of 13 is the lowest radius which fills up the holes
+    dilation_radius = 13
+
+    # Since the shrink/grow can have different radius, we compute the difference between them
+    # and apply it later as erosion/dilation such that the original shape has the same width/height.
+    dilation_erosion_difference = dilation_radius - erosion_radius
+
+    plt.figure(figsize=(16, 6))
+    plt_columns = 1
+    plt_rows = 4
+    plt.subplot(plt_columns, plt_rows, 1)
+    plt.title("Before")
+    plt.imshow(im)
+
+    im = morph.binary_erosion(im, selem=morph.disk(erosion_radius))
+
+    plt.subplot(plt_columns, plt_rows, 2)
+    plt.title("After erosion (shrink)")
+    plt.imshow(im)
+
+    im = morph.binary_dilation(im, selem=morph.disk(dilation_radius))
+
+    plt.subplot(plt_columns, plt_rows, 3)
+    plt.title("After dilation (grow)")
+    plt.imshow(im)
+
+    if(dilation_erosion_difference > 0):
+        im = morph.binary_erosion(
+            im, selem=morph.disk(dilation_erosion_difference))
+
+        plt.subplot(plt_columns, plt_rows, 4)
+        plt.title("After second erosion (shrink)")
+        plt.imshow(im)
+    elif(dilation_erosion_difference < 0):
+        im = morph.binary_dilation(
+            im, selem=morph.disk(dilation_erosion_difference))
+
+        plt.subplot(plt_columns, plt_rows, 4)
+        plt.title("After second dilation (grow)")
+        plt.imshow(im)
+
+    # plt.savefig("image_processed/task3a_pyplot.png")
+    # plt.show()
+
     return im
     ### END YOUR CODE HERE ###
 
